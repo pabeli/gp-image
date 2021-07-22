a09994589fbb0c3af947e2ba4f028b7c245432b2
pabeli <limapaulabelen@gmail.com>
2021-07-22 11:14:10 -0300

    not acepting blank spaces

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseBadRequest
+import re
 
 # Check MIME type
 def check_mime(file):
@@ -10,10 +10,18 @@ def check_mime(file):
 
 @csrf_exempt
 def post_image(request):
-    image_file = request.FILES['image_file'].file.read()
-    mime_type = request.FILES['image_file'].content_type
-    Post.objects.create(image=image_file, mime_type=mime_type)
-    return HttpResponse(status=204)
+    image_name = request.FILES['image_file'].name
+    blank_spaces = re.findall(r'[^\S\n\t]+',image_name)
+    if len(blank_spaces) == 0:
+        image_file = request.FILES['image_file'].file.read()
+        mime_type = request.FILES['image_file'].content_type
+        Post.objects.create(image=image_file, mime_type=mime_type)
+        return HttpResponse(status=204)
+    else:
+        return HttpResponseBadRequest(
+            'The name of the image contains blank spaces. Call Taylor Swift to fill them',
+            status=400)
+
 
 def get_image(request, pk):
     post = Post.objects.get(id=pk)
